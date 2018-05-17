3772d364f9ab6ceedfd6b52a8aa6cd4a1ac035ba
Nikita Grishko <gr1n@protonmail.com>
2018-05-17 17:47:33 +0300

    Preparing release 0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 Changelog for json-ext-encoder
 ==============================
 
-0.1.0 (unreleased)
+0.1.0 (2018-05-17)
 ------------------
 
 - Initial implementation. Supported types: `datetime.datetime`, `datetime.date`, `datetime.time`, `datetime.timedelta`, `decimal.Decimal`, `uuid.UUID` and `enum.Enum`.
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='json-ext-encoder',
-    version='0.1.0.dev0',
+    version='0.1.0',
     description='Extended JSON encoder for Python data structures',
     long_description=long_description,
     long_description_content_type='text/markdown',
